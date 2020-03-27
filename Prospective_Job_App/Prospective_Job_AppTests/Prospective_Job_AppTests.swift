a7c1801ff6d8933a6998b5f6374f16bd13a5ef91
Lauren Kapraun <38970744+LaurenKapraun@users.noreply.github.com>
2020-03-27 18:44:58 -0500

    Initial xCode Files Created

---
+++ Prospective_Job_App/Prospective_Job_AppTests/Prospective_Job_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prospective_Job_AppTests.swift
+//  Prospective_Job_AppTests
+//
+//  Created by Lauren Kapraun on 3/27/20.
+//  Copyright © 2020 ilstu.edu. All rights reserved.
+//
+
+import XCTest
+@testable import Prospective_Job_App
+
+class Prospective_Job_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
